d3737e5c38e9aec9bf1a9022070bdd5a1137e504
one-inadem2015 <41300423+one-inadem2015@users.noreply.github.com>
2018-09-04 13:46:56 -0500

    Update mdatepicker.js

--- noticas/mdatepicker.js
+++ noticas/mdatepicker.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
   // Set current date
     var date = new Date();
     picker = new Pikaday({
-      field: document.getElementById('datepicker'),
+      field: document.getElementById('datepicker_notas'),
       format: 'D/M/YYYY',
       toString(date, format) {
           const day = date.getDate() ;
@@ -42,7 +42,7 @@ function cargarArticulo(day, month, year) {
   let mMonth = month < 10 ? "0" + month.toString(): month.toString();
   let fileID = year.toString()  + mMonth.toString() + mDay.toString();
   let label = day.toString() + " de " + months[month-1].toString() + " de " + year.toString();
-  $("#datepicker").val(label);
+  $("#datepicker_notas").val(label);
   $.ajax({
     url: baseUrl + year + '/' + mMonth + '/' + fileID + ".json",
     dataType: "JSON",
