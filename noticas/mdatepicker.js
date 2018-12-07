5d6790304f3b9ba9f878bcc22a8d4a6f72056141
one-inadem2015 <41300423+one-inadem2015@users.noreply.github.com>
2018-12-07 15:13:16 -0600

    Update mdatepicker.js

--- noticas/mdatepicker.js
+++ noticas/mdatepicker.js
@@ -2,7 +2,7 @@ var baseUrl = "https://raw.githubusercontent.com/one-inadem2015/repositorio-one/
 var picker = ""; 
 $(document).ready(function () {
   // Set current date
-    var date = new Date(2018,11,05);
+    var date = new Date(2018,11,07);
     picker = new Pikaday({
       field: document.getElementById('datepicker'),
       format: 'DD/MM/YYYY',
